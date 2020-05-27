622587c214a88b1dac6ca5212c22dc12a5d75f97
student <o1073011@gm.pu.edu.tw>
2020-05-27 11:24:53 +0800

    新增一個Java Class

---
+++ app/src/main/java/tw/edu/pu/o1073011/trafficlight/GameSurfaceView.java
@@ -0,0 +1,11 @@
+package tw.edu.pu.o1073011.trafficlight;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class GameSurfaceView extends SurfaceView {
+    public GameSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
